fc522fa9721df6cd09fa16c2bd0755a22454110a
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-08-01 15:39:45 -0700

    Adding sub-form for get

--- src/assets/testing.js
+++ src/assets/testing.js
@@ -11,8 +11,8 @@ function setRequestBar(requestType) {
   switch (requestType) {
     case "get":
       console.log(requestType);
-      document.getElementById("demo").innerHTML =
-        "...personal-site/resource?type={resume/projects/interests}";
+      setGetForm();
+      document.getElementById("test").innerHTML = document.getElementById("object-choice").value;
       break;
     case "post":
       console.log(requestType);
@@ -27,4 +27,25 @@ function setRequestBar(requestType) {
       console.log(requestType);
       break;
   }
+}
+
+function setGetForm() {
+  document.getElementById("demo").innerHTML = "...personal-site/resource?type=";
+  form = document.createElement("form");
+  form.id = "request-object";
+  select = document.createElement("select");
+  select.id = "object-choice";
+  form.appendChild(select);
+
+  option1 = document.createElement("option");
+  option1.value = 'resume';
+  option1.innerHTML = "Resume";
+  select.appendChild(option1);
+
+  option2 = document.createElement("option");
+  option2.value = 'projects';
+  option2.innerHTML = "Projects";
+  select.appendChild(option2);
+
+  document.getElementById("demo").appendChild(form);
 }
