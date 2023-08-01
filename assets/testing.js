1262dac1e4c8231bf70b679977fbe97f2d9bfb41
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-08-01 13:58:26 -0700

    adding user request type, troubleshooting subsequent render

--- assets/testing.js
+++ assets/testing.js
@@ -15,11 +15,35 @@
 // }
 
 document.getElementById("title-name").innerHTML = "Hi, I'm Ali Ladha";
-document.getElementById("demo").innerHTML = "hi";
+// document.getElementById("demo").innerHTML = "hi";
 // var btn = document.getElementById('submit');
 // btn.addEventListener('click', func);
-document.getElementById("submit").addEventListener("click", function () {
+document.getElementById("submit-type").addEventListener("click", function () {
   event.preventDefault();
-  document.getElementById("title-name").innerHTML =
-    "Hi, I'm " + document.getElementById("input-name").value;
+  document.getElementById("demo").innerHTML = document.getElementById("request-type").value;
+  setRequestBar(document.getElementById("request-type").value);
+  
+  
+  
 });
+
+function setRequestBar(requestType){
+  switch(requestType) {
+    case "get":
+      console.log(requestType);
+      document.getElementById("demo").innerHTML = "...personal-site/resource?type={resume/projects/interests}";
+      break;
+    case "post":
+      console.log(requestType);
+      break;
+    case "patch":
+      console.log(requestType);
+      break;
+    case "put":
+      console.log(requestType);
+      break;
+    case "delete":
+      console.log(requestType);
+      break;
+  }
+}
