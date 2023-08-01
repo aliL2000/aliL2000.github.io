0f175be22a71c4887f093e5d530925968f2ac2a7
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-08-01 14:50:41 -0700

    Adding formatting

--- src/assets/testing.js
+++ src/assets/testing.js
@@ -1,37 +1,18 @@
-// window.onload = function () {
-//   default_values();
-// };
-
-// function default_values() {
-//   document.getElementById("title-name").innerHTML = "Hi, I'm Ali Ladha";
-//   document.getElementById("demo").innerHTML = "hi";
-//   // var btn = document.getElementById('submit');
-//   // btn.addEventListener('click', func);
-//   document.getElementById("submit").addEventListener("click", function () {
-//     event.preventDefault();
-//     document.getElementById("title-name").innerHTML =
-//       "Hi, I'm " + document.getElementById("input-name").value;
-//   });
-// }
-
 document.getElementById("title-name").innerHTML = "Hi, I'm Ali Ladha";
-// document.getElementById("demo").innerHTML = "hi";
-// var btn = document.getElementById('submit');
-// btn.addEventListener('click', func);
+
 document.getElementById("submit-type").addEventListener("click", function () {
   event.preventDefault();
-  document.getElementById("demo").innerHTML = document.getElementById("request-type").value;
+  document.getElementById("demo").innerHTML =
+    document.getElementById("request-type").value;
   setRequestBar(document.getElementById("request-type").value);
-  
-  
-  
 });
 
-function setRequestBar(requestType){
-  switch(requestType) {
+function setRequestBar(requestType) {
+  switch (requestType) {
     case "get":
       console.log(requestType);
-      document.getElementById("demo").innerHTML = "...personal-site/resource?type={resume/projects/interests}";
+      document.getElementById("demo").innerHTML =
+        "...personal-site/resource?type={resume/projects/interests}";
       break;
     case "post":
       console.log(requestType);
