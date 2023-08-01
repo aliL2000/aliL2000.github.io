bfa5d1d61d34a0aa1af72957c0a827137821773f
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-07-31 18:34:41 -0700

    adding user input and re-formatting js import

--- assets/testing.js
+++ assets/testing.js
@@ -1,7 +1,25 @@
-window.onload = function () {
-  whatis();
-};
+// window.onload = function () {
+//   default_values();
+// };
 
-function whatis() {
-  document.getElementById("demo").innerHTML = "hi";
-}
+// function default_values() {
+//   document.getElementById("title-name").innerHTML = "Hi, I'm Ali Ladha";
+//   document.getElementById("demo").innerHTML = "hi";
+//   // var btn = document.getElementById('submit');
+//   // btn.addEventListener('click', func);
+//   document.getElementById("submit").addEventListener("click", function () {
+//     event.preventDefault();
+//     document.getElementById("title-name").innerHTML =
+//       "Hi, I'm " + document.getElementById("input-name").value;
+//   });
+// }
+
+document.getElementById("title-name").innerHTML = "Hi, I'm Ali Ladha";
+document.getElementById("demo").innerHTML = "hi";
+// var btn = document.getElementById('submit');
+// btn.addEventListener('click', func);
+document.getElementById("submit").addEventListener("click", function () {
+  event.preventDefault();
+  document.getElementById("title-name").innerHTML =
+    "Hi, I'm " + document.getElementById("input-name").value;
+});
