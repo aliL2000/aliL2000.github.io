ba4f5ed9e49e8a52d26537f76b61096b9c67854b
aliL2000 <aliladha2000@gmail.com>
2023-09-21 14:30:51 -0400

    adding descriptions

--- src/routes/get/render_projects.js
+++ src/routes/get/render_projects.js
@@ -1,17 +1,15 @@
-/*
-function for:
-over-all div, accept (name, paragraph)
-within this
-    render tech-stack (input arr, length, list)
-
-*/
-
 //Create const to hold the data that I will be adding to the page
 const projectNames = [
   "LumberViz",
   "ROSEN Capstone",
   "Canvas Gamification",
   "Neetcode 150",
+];
+const projectLinks = [
+  "https://github.com/aliL2000/olympic",
+  "https://github.com/Rosen-Team-B/Rosen_B_2",
+  "",
+  "https://github.com/aliL2000/leetcode-practice",
 ];
 const paragraphs = [
   "Created an EDA tool that allows simultaneous filtering and visualization, providing users with insightful tools for data exploration.",
@@ -30,24 +28,28 @@ function createProjects() {
   //Create each project as it's own seperate object
   var project1 = createProject(
     projectNames[0],
+    projectLinks[0],
     paragraphs[0],
     4,
     technologies[0]
   );
   var project2 = createProject(
     projectNames[1],
+    projectLinks[1],
     paragraphs[1],
     3,
     technologies[1]
   );
   var project3 = createProject(
     projectNames[2],
+    projectLinks[2],
     paragraphs[2],
     2,
     technologies[2]
   );
   var project4 = createProject(
     projectNames[3],
+    projectLinks[3],
     paragraphs[3],
     2,
     technologies[3]
@@ -61,9 +63,26 @@ function createProjects() {
   return projects;
 }
 
-function createProject(name, description, arrayLength, technologies) {
+function createProject(name, url, description, arrayLength, technologies) {
   var project = document.createElement("div");
-  project.innerHTML = name;
+  //Create project name
+  var projectName  = document.createElement("h1");
+  projectName.id = "project-container";
+  projectName.innerHTML = name;
+  project.append(projectName)
+  //Appending the unique link to each github project, if url is provided
+  if (url !== "") {
+    var link = document.createElement("a");
+    link.textContent = "Visit My GitHub Repository";
+    link.href = url;
+    link.target = "_blank";
+    project.append(link);
+  }
+  //Add descriptions
+  var projectdescription = document.createElement("p");
+  projectdescription.id = "description-container";
+  projectdescription.innerHTML = description;
+  project.append(projectdescription);
   return project;
 }
 
