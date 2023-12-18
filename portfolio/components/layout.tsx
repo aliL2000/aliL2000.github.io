1d7dd1bf50228576e06b9984da823d061f5e2f27
aliL2000 <aliladha2000@gmail.com>
2023-12-18 18:41:34 -0500

    Updating css routing and adding footer and project compon

--- portfolio/components/layout.tsx
+++ portfolio/components/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Footer from './footer';
+import Projects from './projects'
  
 export default function Layout({ children }) {
   return (
     <>
-      <main>{children}</main>
+      <main>
+        {children}
+        <Projects />
+      </main>
       <Footer />
     </>
   )
---
+++ portfolio/components/projects.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/footer.module.css";
+
+const Projects = () => {
+  return (
+    <h1 className={styles.title}>Projects</h1>
+  );
+};
+
+export default Projects;
